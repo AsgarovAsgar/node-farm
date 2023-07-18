25a67dc0c733d34487161a587b47e69360bfc28a
Asgarov Asgar <asgara@maestro.st>
2023-07-18 23:58:33 +0400

    simple routing

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fs = require('fs') // file system
+const http = require('http')
+const url = require('url')
 
+////////////////////
+// FILES
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
@@ -10,19 +14,44 @@ const fs = require('fs') // file system
 // console.log('doc created');
 
 // Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if(err) return console.log('Error! ', err);
-  console.log(data1);
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      console.log(data3);
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if(err) return console.log('Error! ', err);
+//   console.log(data1);
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('Your file has been written');
-      })
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your file has been written');
+//       })
+//     })
+//   })
+// })
+
+// console.log('will read file');
+
+/////////////////
+// SERVER
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url
+
+  if(pathName === '/overview') {
+    res.end('this is overview')
+  } else if(pathName === '/product') {
+    res.end('this is product')
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
     })
-  })
+    res.end('not found')
+  }
+
 })
 
-console.log('will read file');
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listen to req on port 8000');
+})
+
